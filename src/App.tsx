d3357aba4f3425aae34ff1b84716ebd240313de2
SevgiChavushoglu <sevgi2308@gmail.com>
2023-07-13 10:48:44 +0300

    build fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { initialiseAppState } from "./utils/initialiseAppState";
